ed8fa7d0ef9ff75b4e4368df5cfb2861f2ba92be
Diego Pereyra <8pereyradiego8@gmail.com>
2021-03-11 17:46:28 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { NavBar } from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://es.reactjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprende React
-        </a>
-      </header>
+      <NavBar />
+      <h2>Bienvenido</h2>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const NavBar = () => {
+  return ( <div>
+    <h3>DP Componentes</h3>
+    <ul>
+      <li>
+        <a href="">Motherboards</a>
+      </li>
+      <li>
+        <a href="">Procesadores</a>
+      </li>
+      <li>
+        <a href="">RAMs</a>
+      </li>
+      <li>
+        <a href="">SSDs</a>
+      </li>
+    </ul>
+    </div>
+  )
+}
